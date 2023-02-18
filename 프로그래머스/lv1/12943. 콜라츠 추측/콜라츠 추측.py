577a888ac02e7a38edc7d422cc216e790a238baa
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2023-02-18 19:31:24 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 9.95 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,11 @@
+def solution(n):
+    cnt = 0
+    while n != 1:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = (n * 3) + 1
+        cnt += 1
+        if cnt >= 500:
+            return -1
+    return cnt
