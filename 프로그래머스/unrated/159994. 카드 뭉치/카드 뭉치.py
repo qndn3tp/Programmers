d07cc44defb8481f116c0fb068b4ae83d586d096
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2023-03-09 22:07:31 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -1,14 +1,13 @@
+from collections import deque
 def solution(cards1, cards2, goal):
-    c1, c2 = 0, 0
-    for i in goal:
-        if i in cards1:
-            if cards1.index(i) > c1:
-                return "No"
-            else:
-                c1 += 1
-        elif i in cards2:
-            if cards2.index(i) > c2:
-                return "No"
-            else:
-                c2 += 1
-    return "Yes"
+    card1_q = deque(cards1)
+    card2_q = deque(cards2)
+    ans = "Yes"
+    for word in goal:
+        if card1_q and card1_q[0] == word:
+            card1_q.popleft()
+        elif card2_q and card2_q[0] == word:
+            card2_q.popleft()
+        else:
+            ans = "No"
+    return(ans)
