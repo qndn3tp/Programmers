726f45103de1212fe3f4e73ede75a3ca143a7f73
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2023-03-10 19:55:50 +0900

    [unrated] Title: 문자열 나누기, Time: 1.82 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/unrated/140108. 문자열 나누기/문자열 나누기.py
@@ -1,19 +1,20 @@
 def solution(s):
-    x = s[0]
-    x_cnt, y_cnt = 0 , 0
-
+    t = ["", 0, 0] # 첫번째 문자, 같은 문자 카운트, 다른 문자 카운트
     ans = 0
-    for i in range(len(s)):
-        if s[i] == x:   # x와 같은 글자 카운트
-            x_cnt += 1
-        else:           # 다른 글자 카운트
-            y_cnt += 1
-        if x_cnt != 0 and x_cnt == y_cnt:
-            ans += 1
-            x_cnt, y_cnt = 0 , 0
-            if i < len(s)-1:
-                x = s[i+1]    
-        # 두 횟수가 다른 상태에서 더 읽은 글자가 없는 경우
-        elif x_cnt != y_cnt and i == len(s)-1:
-            ans += 1
-    return(ans)
+    for i in s:
+        # 첫번째 문자 x 지정
+        if t[0] == "":
+            t[0] = i
+            t[1] += 1
+        else:
+            if t[0] == i:
+                t[1] += 1
+            else:
+                t[2] += 1
+            if t[1] == t[2]:
+                ans += 1
+                t = ["", 0, 0]
+    # 두 횟수가 다른 상태에서 더 읽을 글자가 없는 경우
+    if t != ["", 0, 0]:
+        ans += 1
+    return ans
