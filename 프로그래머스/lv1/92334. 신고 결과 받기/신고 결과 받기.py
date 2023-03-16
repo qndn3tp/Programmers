bd1294ebc8e322bbcb069bb5135b968bb8bfe9a1
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2023-03-17 01:11:43 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,36 +1,13 @@
 def solution(id_list, report, k):
-    rp = [user.split(" ") for user in set(report)]
 
+    answer = [0] * len(id_list)
+    reports = {x: 0 for x in id_list}
 
-    # 신고받은 이용자 리스트(id-신고당한 횟수)
-    dic = {}
-    for id in id_list:
-        dic[id] = 0
-    for rp_user in rp:
-        if rp_user[1] in dic:
-            dic[rp_user[1]] += 1
-    
-    # 정지되야할 이용자
-    banned = []
-    for i in dic:
-        if dic[i] >= k:
-            banned.append(i)
-            
-    # 유저id-유저가 신고한id
-    user_dic = {}
-    for user in rp:
-        if user[0] not in user_dic:
-            user_dic[user[0]] = [user[1]]
-        else:
-            if user[1] not in user_dic[user[0]]:
-                user_dic[user[0]].append(user[1])
+    for r in set(report):
+        reports[r.split()[1]] += 1
 
-    # 결과
-    res = {}
-    for i in id_list:
-        res[i] = 0
-    for rp_user in rp:
-        if rp_user[1] in banned:
-            res[rp_user[0]] += 1
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
 
-    return(list(res.values()))
+    return answer
