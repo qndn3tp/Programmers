878fb3d01cd57d294fa0de647a00680065c2bd75
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2023-03-19 17:10:38 +0900

    [level 2] Title: 올바른 괄호, Time: 11.32 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,14 @@
+def solution(s):
+    stack = []
+    for i in s:
+        if i == "(": 
+            stack.append(i)
+        elif i == ")":
+            if len(stack) > 0 and stack[-1] == "(": 
+                stack.pop()
+            else: 
+                stack.append(i)
+    if len(stack) == 0: 
+        return True
+    else: 
+        return False
