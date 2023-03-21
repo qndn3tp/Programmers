0c6cc0af49c35cdf2b7b88e8af53c7cfa7b20964
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2023-03-21 22:02:17 +0900

    [level 2] Title: 숫자의 표현, Time: 3.80 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,16 +1,14 @@
-def findFinn(n, idx):
-    sum = 0
-    for j in range(idx, n//2+2):
-        sum += j
-        if sum == n:
-            return sum
-        elif sum > n:
-            break
-    return 
-
 def solution(n):
+    ls = [i for i in range(1, n//2+2)]
+
     cnt = 1
-    for i in range(1, n//2+1):
-        if findFinn(n, i) == n:
-            cnt += 1
+    for i in range(1, len(ls)): 
+        sum = 0
+        for j in range(i, len(ls)+1): 
+            sum += j
+            if sum == n:
+                cnt += 1
+                break
+            elif sum > n:
+                break
     return(cnt)
