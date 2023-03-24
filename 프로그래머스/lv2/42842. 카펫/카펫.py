e12d01bd0b64310163fb8d43f8239a05bc6e5a52
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2023-03-24 13:58:56 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -3,8 +3,6 @@ def solution(brown, yellow):
     size = [] 
     for i in range(1, int(yellow**0.5)+1):
         if yellow % i == 0:
-            size.append([yellow//i, i])
-    # 그 경우들 중 brown을 만족하는 경우
-    for w, h in size: 
-        if (w * 2) + (h * 2) + 4 == brown:
-            return([w+2, h+2])
+            w, h = yellow // i, i
+            if (w * 2) + (h * 2) + 4 == brown:
+                return([w+2, h+2])
