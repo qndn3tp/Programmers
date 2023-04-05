d970f5fff24874075d511e41c31b4dd66eae85d6
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2023-04-05 22:27:53 +0900

    [unrated] Title: 귤 고르기, Time: 14.97 ms, Memory: 18.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,18 +1,11 @@
-def solution(k, tangerine):
-    # 직접 dic 생성: 귤 크기-귤 개수
-    dic = {}
-    for i in tangerine:
-        if i in dic:
-            dic[i] += 1
-        else:
-            dic[i] = 1
-    # value를 기준으로 정렬, 파악
-    ls = sorted(dic.values(), reverse=True)
+from collections import Counter
 
-    res = 0
-    n_tangerine = 0
-    for v in ls:
-        if n_tangerine < k:
-            n_tangerine += v
+def solution(k, tangerine):
+    counter = Counter(tangerine) # 귤의 크기-귤의 개수
+    res = 0    
+    n = 0       
+    for v in sorted(counter.values(), reverse=True):
+        if n < k:
+            n += v
             res += 1
     return(res)
