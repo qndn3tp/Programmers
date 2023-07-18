c58434ac00290bd1610fe0cf9b1d679f23f144fc
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2023-07-18 21:54:59 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,16 +1,22 @@
 import sys
-T = int(sys.stdin.readline())
 
-def dp(n):
+T = int(sys.stdin.readline())
+tc = [int(sys.stdin.readline().strip()) for _ in range(T)]
+
+for n in tc:
+    # 1,2,3은 규칙에서 제외됨. 따로 추가해줘야함.
     if n == 1:
-        return(1)
+       print(1)
     elif n == 2:
-        return(2)
+        print(2)
     elif n == 3:
-        return(4)
+        print(4)
+    
     else:
-        return dp(n-1) + dp(n-2) + dp(n-3) #
-        
-for i in range(T):
-    n = int(sys.stdin.readline())
-    print(dp(n))
+        dp = [0] * (n+1)    # 0부터 n까지 저장해야되기 때문.
+        dp[1] = 1
+        dp[2] = 2
+        dp[3] = 4
+        for i in range(4, n+1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        print(dp[-1])
