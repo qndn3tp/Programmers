2adf530c8f22e5f4185fbc0b1db3a78b098879c4
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2023-08-27 23:53:19 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 852 ms, Memory: 118828 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,21 +1,16 @@
 import sys
+from collections import Counter
 
 N = int(sys.stdin.readline())
 cards = list(map(int, sys.stdin.readline().split()))
-
 M = int(sys.stdin.readline())
 arr = list(map(int, sys.stdin.readline().split()))
 
 # 카드 숫자 : 개수 딕셔너리
-cards_cnt = {}
-for c in cards:
-    if c in cards_cnt:
-        cards_cnt[c] += 1
-    else:
-        cards_cnt[c] = 1
+counter = Counter(cards)
 
 for i in arr:
-    if cards_cnt.get(i):
-        print(cards_cnt[i], end=" ")
+    if counter.get(i):
+        print(counter[i], end=" ")
     else:
         print(0, end=" ")
