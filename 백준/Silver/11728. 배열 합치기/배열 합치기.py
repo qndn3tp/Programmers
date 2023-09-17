943bbbdee69860062763df8f424878b5bf790483
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2023-09-17 20:20:35 +0900

    [Silver V] Title: 배열 합치기, Time: 1152 ms, Memory: 297560 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.py
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -5,21 +5,4 @@ n, m = map(int, sys.stdin.readline().split())
 a = list(map(int, sys.stdin.readline().split()))
 b = list(map(int, sys.stdin.readline().split()))
 
-res = []
-
-i = 0   # a의 인덱스
-j = 0   # b의 인덱스
-while i < len(a) and j < len(b):
-    if a[i] <= b[j]:
-        res.append(a[i])
-        i += 1
-    else:
-        res.append(b[j])
-        j += 1
-
-if i < len(a) and j == len(b):
-    res += a[i:]
-elif i == len(a) and j < len(b):
-    res += b[j:]
-
-print(" ".join(map(str, res)))
+print(" ".join(map(str, sorted(a+b))))
