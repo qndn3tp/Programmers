1461343cffe69432bbed45ce2288aae9e3d74320
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2024-10-01 02:50:24 +0900

    [Silver III] Title: 회전하는 큐, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,29 +1,30 @@
 import sys
 
-n,m = map(int, sys.stdin.readline().split())
-target = list(map(int, sys.stdin.readline().split()))[::-1]     # 뽑아내려고 하는 수의 위치. 큐는 원소를 앞에 못넣기 때문에 역순으로 타겟팅
+def main():
+    N, M = map(int, sys.stdin.readline().strip().split())
+    target = list(map(int, sys.stdin.readline().strip().split()))
 
-res = 0     # 2,3번 연산 횟수
+    ls = [i for i in range(1, N+1)]
 
-queue = [i for i in range(1, n+1)]      
+    res = 0
+    for t in target:
+        i = ls.index(t)
+        # 1번
+        if ls[0] == t:
+            ls = ls[1:]
+            continue
+        # 왼쪽 | 오른쪽까지의 최소거리
+        # 왼쪽 회전
+        if i < len(ls) - i:
+            res += i
+            tmp = ls[:i]
+            ls = ls[i+1:] + tmp
+        # 오른쪽 회전
+        else:
+            res += len(ls) - i
+            tmp = ls[i+1:]
+            ls = tmp + ls[:i]
+    print(res)
 
-while len(queue) > (n-m) and len(target) > 0:
-    t = target.pop()
-    idx = queue.index(t)
-    # 맨 왼쪽에 위치하면 (1번 연산)
-    if idx == 0:
-        queue.pop(0)
-    # 중간보다 앞쪽에 위치하면 (2번 연산)
-    elif idx <= len(queue)//2:
-        temp = queue.pop(0)
-        queue.append(temp)
-        res += 1
-        target.append(t)
-    # 중간보다 뒷쪽에 위치하면 (3번 연산)
-    else:
-        temp = [queue.pop()]
-        queue = temp + queue
-        res += 1
-        target.append(t)
-
-print(res)
+if __name__ == "__main__":
+    main()
