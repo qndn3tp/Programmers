6cd96d2b8dc257331eee39bce277e1d698538e7b
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2024-11-20 12:35:16 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    start, end = 1, l-1
+
+    res = []
+    while start <= end:
+        mid = (start + end) // 2
+
+        cnt = 0
+        for i in range(len(store)-1):
+            if store[i+1] - store[i] > mid:
+                cnt += (store[i+1] - store[i] -1) // mid
+
+        if cnt > m:
+            start = mid + 1
+        else:
+            res.append(mid)
+            end = mid - 1
+    return min(res)
+
+if __name__ == "__main__":
+    n, m, l = map(int, input().rstrip().split())
+    store = [0] + sorted(list(map(int, input().rstrip().split()))) + [l]
+    print(solution())
