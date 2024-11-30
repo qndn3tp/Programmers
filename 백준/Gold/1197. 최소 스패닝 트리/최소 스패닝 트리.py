f9c059a8aba98267e9556175e79ccc179e3bd275
Geonhye Kim <84118129+qndn3tp@users.noreply.github.com>
2024-11-30 18:19:12 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 256 ms, Memory: 57168 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -2,50 +2,54 @@ import sys
 sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
-v, e = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(e)]
-
 ##############
 # Union Find #
 ##############
-# 초기화
-parent = list(range(v+1))
-for i in range(v+1):
-    parent[i] = i
 
-# union
-def union(x, y):
-    x = parent[x]
-    y = parent[y]
-    parent[y] = x
     
-def find(x):
-    if parent[x] == x:
-        return x
-    else:
-        parent[x] = find(parent[x])
-        return parent[x]
+def solution(graph):
+    # UnionFind 초기화
+    parent = list(range(v+1))
+    for i in range(v+1):
+        parent[i] = i
 
-# 1. 그래프 간선을 가중치 오름차순으로 정렬
-graph = sorted(graph, key=lambda x : x[2])
+    def union(x, y):
+        x = parent[x]
+        y = parent[y]
+        parent[y] = x
+        
+    def find(x):
+        if parent[x] == x:
+            return x
+        else:
+            parent[x] = find(parent[x])
+            return parent[x]
 
-# 2. 간선 연결
-total_edges = 0
-total_rank = 0
-for i in graph:
-    if total_edges < v - 1:
-        v1 = i[0]
-        v2 = i[1]
-        rank = i[2]
+    # 1. 그래프 간선을 가중치 오름차순으로 정렬
+    graph = sorted(graph, key=lambda x : x[2])
 
-        # 사이클 탐지
-        v1_parent = find(v1)
-        v2_parent = find(v2)
+    # 2. 간선 연결
+    total_edges = 0
+    total_rank = 0
+    for i in graph:
+        if total_edges < v - 1:
+            v1 = i[0]
+            v2 = i[1]
+            rank = i[2]
 
-        # 사이클 X
-        if v1_parent != v2_parent:
-            union(min(v1, v2), max(v1, v2))
-            total_edges += 1
-            total_rank += rank
+            # 사이클 탐지
+            v1_parent = find(v1)
+            v2_parent = find(v2)
 
-print(total_rank)
+            # 사이클 X
+            if v1_parent != v2_parent:
+                union(min(v1, v2), max(v1, v2))
+                total_edges += 1
+                total_rank += rank
+
+    print(total_rank)
+
+if __name__ == "__main__":
+    v, e = map(int, input().split())
+    graph = [list(map(int, input().split())) for _ in range(e)]
+    solution(graph)
